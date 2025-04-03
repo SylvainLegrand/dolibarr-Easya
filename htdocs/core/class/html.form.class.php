951f9c1668153a1f0a77b0ad1b07fa93e046ca5d
Kamel Khelifa <kkhelifa@open-dsi.fr>
2025-04-03 17:35:29 +0200

    CORE html form: Add parameters for the hook selectProductsListWhere PR #33718

--- htdocs/core/class/html.form.class.php
+++ htdocs/core/class/html.form.class.php
@@ -2803,7 +2803,7 @@ class Form
 			$sql .= " AND p.fk_product_type = 0";
 		}
 		// Add where from hooks
-		$parameters = array();
+		$parameters = array('filterkey' => &$filterkey);
 		$reshook = $hookmanager->executeHooks('selectProductsListWhere', $parameters); // Note that $action and $object may have been modified by hook
 		$sql .= $hookmanager->resPrint;
 		// Add criteria on ref/label
